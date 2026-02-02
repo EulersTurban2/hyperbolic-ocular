119225aaf65dc9de38f787a94f98405ed715533c
EulersTurban2 <gordandjuric66@gmail.com>
2026-02-02 13:29:51 +0100

    first commit

--- app/src/Main.cpp
+++ app/src/Main.cpp
@@ -1,8 +1,17 @@
 #include <engine/core/Engine.hpp>
+#include <MyApp.hpp>
 
 /**
- * Start here...
+ * 1. MyApp class
+ * 2. Controllers (Main, Graphics, GUI, ...)
+ * 3. U engine pravimo promjene kada pravimo nesto sto je na globalnom nivou potrebno  
+ * 4. 
+ * 
  */
+
+
+
 int main(int argc, char **argv) {
-    return 0;
+    auto app = std::make_unique<app::MyApp>();
+    return app->run(argc,argv);
 }
---
+++ app/src/MyApp.cpp
@@ -0,0 +1,6 @@
+#include <MyApp.hpp>
+#include <spdlog/spdlog.h>
+
+void app::MyApp::app_setup() {
+    spdlog::info("Initialized");
+}
